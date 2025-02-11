d8b5b784dfec8967b994a8ff2d0bcf30406bf764
GuptaAdi09 <17aditaya@gmail.com>
2025-02-11 15:48:18 +0530

    for render

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,7 +99,7 @@ DATABASES = {
     }
 }
 ENVIRONMENT = os.getenv("ENVIRONMENT")
-PRODUCTION = True
+PRODUCTION = False
 if PRODUCTION == True or ENVIRONMENT == "production":
     DATABASES = {
         "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
@@ -168,6 +169,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
